5f0559fc2f7142e47de8e3eaaffd80e097c1dbb1
ewilkinsiii <ewilkinsiii@gmail.com>
2019-04-26 13:16:16 -0500

    updated application.rb

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,7 +8,7 @@ Rails.application.config.assets.version = '1.0'
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( blogs.scss)
-ails.application.config.assets.precompile += %w(custom.js plugin.js )
+Rails.application.config.assets.precompile += %w(custom.js plugin.js )
 
 
 # Precompile additional assets.
