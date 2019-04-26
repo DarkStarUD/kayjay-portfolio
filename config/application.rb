be1edaeabe8cd9c62d082656c0313e5e8ce8c03d
ewilkinsiii <ewilkinsiii@gmail.com>
2019-04-26 12:42:03 -0500

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,6 @@ Bundler.require(*Rails.groups)
 module KayProfile
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
-    config.assets.initialize_on_precompile = false
+    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
