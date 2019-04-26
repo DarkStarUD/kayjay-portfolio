93f8c2310916d09705432c2bc92163a017aa7d0a
ewilkinsiii <ewilkinsiii@gmail.com>
2019-04-26 12:50:34 -0500

    updated application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module KayProfile
   class Application < Rails::Application
     config.eager_load_paths << "#{Rails.root}/lib"
     config.secret_key_base = ENV["SECRET_KEY_BASE"]
-    config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
 
