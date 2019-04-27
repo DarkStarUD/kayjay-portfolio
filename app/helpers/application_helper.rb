6982822803164105032441db3f27bb03a70d490d
ewilkinsiii <ewilkinsiii@gmail.com>
2019-04-27 09:19:11 -0500

    fixed login link ans admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,6 @@ module ApplicationHelper
   
     def login_helper style = ''
       if current_user.is_a?(GuestUser)
-        (link_to "Register", root_path, class: style) +
-        " ".html_safe +
         (link_to "Login", new_user_session_path, class: style)
       else
         link_to "Logout", destroy_user_session_path, method: :delete, class: style
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,14 @@ class User < ApplicationRecord
   ## The :user role is added by default and shouldn't be included in this list.             ##
   ## The :root_admin can access any page regardless of access settings. Use with caution!   ##
   ## The multiple option can be set to true if you need users to have multiple roles.       ##
-  petergate(roles: [:admin, :editor], multiple: false)                                      ##
+  petergate(roles: [:site_admin, :editor], multiple: false)                                      ##
   ############################################################################################ 
  
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  petergate(roles: [:site_admin, :editor], multiple: false)    
+         :recoverable, :rememberable, :validatable  
   has_many :comments, dependent: :destroy
   has_many :links, dependent: :destroy  
 end
