dc86edc030a7cd17dab6224c2f2f38b6bebdb9df
ewilkinsiii <ewilkinsiii@gmail.com>
2019-04-27 10:13:57 -0500

    configured action mailer and action cable on prod

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,12 +3,13 @@ class Contact < MailForm::Base
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message,   :validate => true
   attribute :phone,     :validate => true
+  attribute :subject,     :validate => true
   attribute :nickname,  :captcha  => true
   
   def headers
     {
-      :subject => "Portfolio Application Contact Form",
-      :to => "admin@ewilkinsiii.pro",
+      :subject => "Portfolio Application Contact Form:  #{suject}",
+      :to => 'ewilkins@gmail.com',
       :from => %("#{name}" <#{email}>)
     }
   end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://kayjay-portfolio.herokuapp.com', 'ewilkinsiii@gmail.com']
+  config.action_cable.url = "wss://kayjay-portfolio.herokuapp.comcable"
+  
+  config.action_mailer.default_url_options = {host: 'https://kayjay-portfolio.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  
+  ActionMailer::Base.smtp_settings = {
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :domain => 'herokuapp.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 end
